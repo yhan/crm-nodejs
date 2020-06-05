47e9ac50a1b2c6da321a4fe19fdb84647f8952a3
Yi HAN <hanyi@8110@gmail.com>
2020-06-05 17:33:04 +0200

    routes

---
+++ routes/crmRoutes.js
@@ -0,0 +1,22 @@
+const routes = (app) => {
+    app.route("/contact")
+        .get((req, res) => {
+            res.send("GET Hello world");
+        })
+
+        .post((req, res) => {
+            res.send("POST Hello world");
+        });
+
+    // a more funky one
+    app.route("/contact/:contactId")
+        .put((req, res) => {
+            res.send("PUT a contact");
+        })
+
+        .delete((req, res) => {
+            res.send("DELETE a contact");
+        });
+};
+
+export default routes;
